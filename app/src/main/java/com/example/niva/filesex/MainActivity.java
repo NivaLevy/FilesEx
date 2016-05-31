2863628c162c03dc5fb937a2a1f2f29638e4387e
Niva Levi <niva.levi@gmail.com>
2016-05-31 17:32:30 +0300

    adding more meat to this application read-write to file

--- app/src/main/java/com/example/niva/filesex/MainActivity.java
+++ app/src/main/java/com/example/niva/filesex/MainActivity.java
@@ -1,62 +1,46 @@
 package com.example.niva.filesex;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+// background = ResourcesCompat.getDrawable(getResources(), R.drawable.aladdin, null);
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView lblLyrics;
-    Button cmdPlay;
-    MediaPlayer mediaPlayer;
+    public final static int LITTLE_MERMAID = 1, JUNGLE_BOOK = 2;
+    private Button cmdJungleBook, cmdLittleMermaid;
+    private Intent intent;
+    private FilesOperations operations;
+    final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cmdPlay = (Button)findViewById(R.id.cmdPlay);
-        lblLyrics = (TextView)findViewById(R.id.lblLyrics);
-        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        intent = new Intent(getApplicationContext(),DisneySong.class);
+        operations = new FilesOperations();
 
-        cmdPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.start();
-                showLyrics();
-            }
-        });
+        cmdJungleBook = (Button)findViewById(R.id.cmdJungleBook);
+        cmdLittleMermaid = (Button)findViewById(R.id.cmdLittleMermaid);
 
+        cmdJungleBook.setOnClickListener(this);
+        cmdLittleMermaid.setOnClickListener(this);
 
-    }
-
-    private void showLyrics() {
-        InputStream inputStream = getResources().openRawResource(R.raw.lyrics);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String str = "", lyrics = "";
-
-
-        try {
-            while ((str = bufferedReader.readLine()) != null){
-                lyrics += str + "\n";
-            }
-            inputStream.close();
-            lblLyrics.setText(lyrics);
-        }catch (IOException e){
-            lyrics = "";
-        }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        mediaPlayer.stop();
+    public void onClick(View v) {
+        Button b = (Button)v;
+        if(v.equals(cmdLittleMermaid))
+            intent.putExtra("type", LITTLE_MERMAID);
+        if(v.equals(cmdJungleBook))
+            intent.putExtra("type", JUNGLE_BOOK);
+        String nameOfSong = b.getText().toString();
+        operations.writeToFile(nameOfSong, getApplicationContext());
+        Log.i(TAG, operations.readFromFile(getApplicationContext()));
+        startActivity(intent);
     }
 }
